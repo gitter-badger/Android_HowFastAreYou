116fd41a1d711f538e6a86f250e2c161773442c8
jupiter2359media <cao.vu@2359media.com.vn>
2015-10-15 16:13:05 +0700

    [ADD] Add skeleton code for settings page.

--- app/src/main/java/com/hasbrain/howfastareyou/TapCountActivity.java
+++ app/src/main/java/com/hasbrain/howfastareyou/TapCountActivity.java
@@ -1,8 +1,11 @@
 package com.hasbrain.howfastareyou;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
@@ -33,14 +36,24 @@ public class TapCountActivity extends AppCompatActivity {
             public void onChronometerTick(Chronometer chronometer) {
                 if (SystemClock.elapsedRealtime() - startTime >= TIME_COUNT) {
                     pauseTapping();
-                    postResult();
                 }
             }
         });
     }
 
-    private void postResult() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_settings) {
+            Intent showSettingsActivity = new Intent(this, SettingsActivity.class);
+            startActivity(showSettingsActivity);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @OnClick(R.id.bt_start)
